6321d0de7f495510e25e41bc9f6c17f0c766adb7
sirimala <sirimala.sreenath@gmail.com>
2015-12-08 22:31:32 +0530

    logout
    logout updated

--- tic_tac_toe_app/urls.py
+++ tic_tac_toe_app/urls.py
@@ -8,5 +8,8 @@ urlpatterns = patterns('',
     url(r'^register/$', register),
     url(r'^register/success/$', register_success),
     url(r'^home/$', home),
+    url(r'^game/(?P<id>\d+)/', game),
+    url(r'^game/$', game),
+
     
 )
